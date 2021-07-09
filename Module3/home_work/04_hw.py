09d0151238c3e17a5cfcc8ce2d2468d9c278402f
Gemberg Andrey <87034537+AndreyJZ@users.noreply.github.com>
2021-07-09 13:52:24 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,19 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+numbers1 = []
+numbers2 = []
+n = int(input('Введите число элементов: '))
+sm = 0
+
+for el_1 in range(n):
+    numbers1.append(random.randint(-100, 100))
+for el_2 in numbers1:
+    if el_2 > 0 and el_2**0.5*10 % 10 == 0:
+        numbers2.append(el_2)
+        
+print(numbers1)
+print()
+print(numbers2)
